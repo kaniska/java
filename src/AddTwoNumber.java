6bae9e5f0d5a732899ed6b3074d4001fdd08267a
mindpower <bu6@me.com>
2012-04-13 06:33:19 -0700

    Update src/AddTwoNumber.java

--- src/AddTwoNumber.java
+++ src/AddTwoNumber.java
@@ -13,11 +13,11 @@ public class Solution {
     public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
         if (l1==null) return l2;
         if (l2==null) return l1;
-        ListNode head = new ListNode((l1.val+l2.val)%10);
-        int v1, v2, carry=(l1.val+l2.val)/10;
+        int v1=l1.val, v2=l2.val, carry=(v1+v2)/10;
+        ListNode head = new ListNode((v1+v2)%10);
+        ListNode prev = head;
         l1 = l1.next;
         l2 = l2.next;
-        ListNode prev = head;
         while (l1!=null || l2!=null) {
             if (l1==null) v1=0;
             else {
