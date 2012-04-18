e4d5ab7c557c69dce62a444c8fb852a227f1cbae
Mind Power <bu6@me.com>
2012-04-17 22:11:03 -0700

    length of last word

---
+++ leetcode/LengthOfLastWord.java
@@ -0,0 +1,11 @@
+public class Solution {
+    public int lengthOfLastWord(String s) {
+        if (s==null || s.length()==0) return 0;
+        int end=s.length()-1;
+        while (end>=0 && s.charAt(end)==' ') end--;
+        if (end==-1) return 0;
+        int start=end-1;
+        while (start>=0 && s.charAt(start)!=' ') start--;
+        return end-start;
+    }
+}
