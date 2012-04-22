4690ab92da1660cf2e59402bd9f10fd2e40cf939
Mind Power <bu6@me.com>
2012-04-22 16:56:09 -0700

    04/22/2012

--- fb/string/RegularExpression.java
+++ fb/string/RegularExpression.java
@@ -7,28 +7,33 @@ package fb.string;
  * @author jbu
  */
 public class RegularExpression {
-
   public boolean isMatch(String s, String p) {
-    if (p == null && s == null) return true;
-    if (p == null || s == null) return false;
+    if (s == null && p == null) return true;
+    if (s == null || p == null) return false;
     if (p.equals(".*")) return true;
     if (p.equals("")) return s.equals("");
     return doMatch(s, p, 0, 0);
   }
 
-  boolean doMatch(String s, String p, int ps, int pp) {
-    int ls = s.length(), lp = p.length();
-    if (pp == lp) return ps == ls;
-    // when p reach end OR p next is not *
-    if (pp == lp - 1 || p.charAt(pp + 1) != '*') {
-      if (ps > ls - 1) return false;
-      return (s.charAt(ps) == p.charAt(pp) || p.charAt(pp) == '.') && doMatch(s, p, ps + 1, pp + 1);
+  boolean doMatch(String s, String p, int ls, int lp) {
+    if (lp == p.length()) return ls == s.length();
+    if (lp == p.length() - 1 || p.charAt(lp + 1) != '*') { // not wildcard
+      return ls < s.length() && (p.charAt(lp) == '.' || s.charAt(ls) == p.charAt(lp))
+          && doMatch(s, p, ls + 1, lp + 1);
     }
-    // when p next is *
-    while (ps < ls && (s.charAt(ps) == p.charAt(pp) || p.charAt(pp) == '.')) {
-      if (doMatch(s, p, ps, pp + 2)) return true;
-      ps++;
+    // if next char is *
+    // first we try simply skip the next char+'*', (matches 0 chars in s)
+    if (doMatch(s, p, ls, lp + 2)) return true;
+    // then we try to match 1 and more chars in s and see if the remaining can be matched
+    while (ls < s.length() && (p.charAt(lp) == '.' || s.charAt(ls) == p.charAt(lp))) {
+      if (doMatch(s, p, ls + 1, lp + 2)) return true;
+      ls++;
     }
-    return doMatch(s, p, ps, pp + 2);
+    // if none can be matched, return false
+    // this changes a little over leetcode, but I think it's easier to understand, which
+    // inside the while, uses ls instead of ls+1.
+    // For example, if .* matches 0..5 in s, that will match 0..4 first and then the last case;
+    // However the code is a little hard to understand
+    return false;
   }
 }
