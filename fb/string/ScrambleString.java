16b61b6c445d23fd1306b9deaa6af9fd0a2ee0cd
Mind Power <bu6@me.com>
2012-05-18 20:46:10 -0700

    update

--- fb/string/ScrambleString.java
+++ fb/string/ScrambleString.java
@@ -45,51 +45,29 @@ package fb.string;
  */
 public class ScrambleString {
   public boolean isScramble(String s1, String s2) {
-    if (s1.equals(s2)) {
-      return true;
-    }
-    char[] c1 = s1.toCharArray();
-    char[] c2 = s2.toCharArray();
-    return isScramble(c1, 0, c1.length, c2, 0, c2.length);
+    //       if (s1.equals(s2)) return true;
+    return isScramble(s1, 0, s1.length(), s2, 0, s2.length());
   }
 
-  boolean isScramble(char[] c1, int start1, int end1, char[] c2, int start2, int end2) {
-    if (!isSameCharSet(c1, start1, end1, c2, start2, end2)) {
-      return false;
-    }
-    if (end1 - start1 == 1) {
-      return c1[start1] == c2[start2];
-    }
-    for (int i = 1; i < end1 - start1; i++) {
-      if (isScramble(c1, start1, start1 + i, c2, start2, start2 + i) &&
-          isScramble(c1, start1 + i, end1, c2, start2 + i, end2))
-      {
-        return true;
-      }
-      if (isScramble(c1, start1, start1 + i, c2, end2 - i, end2) &&
-          isScramble(c1, start1 + i, end1, c2, start2, end2 - i))
-      {
-        return true;
-      }
+  boolean isScramble(String s1, int l1, int r1, String s2, int l2, int r2) {
+    if (!isSameCharSet(s1, l1, r1, s2, l2, r2)) return false;
+    if (r1 == l1 + 1) return true;
+    for (int i = 1; i < r1 - l1; i++) {
+      if (isScramble(s1, l1, l1 + i, s2, l2, l2 + i) && isScramble(s1, l1 + i, r1, s2, l2 + i, r2)) return true;
+      // the second compare range are very tricky and I got it wrong several times
+      // better draw picture
+      if (isScramble(s1, l1, l1 + i, s2, r2 - i, r2) && isScramble(s1, l1 + i, r1, s2, l2, r2 - i)) return true;
     }
     return false;
   }
 
-  boolean isSameCharSet(char[] c1, int start1, int end1, char[] c2, int start2, int end2) {
-    int[] charCount = new int[256];
-    if (end1 - start1 != end2 - start2) {
-      return false;
-    }
-    for (int i = start1; i < end1; i++) {
-      charCount[c1[i]]++;
-    }
-    for (int i = start2; i < end2; i++) {
-      charCount[c2[i]]--;
-    }
-    for (int i = 0; i < charCount.length; i++) {
-      if (charCount[i] != 0) {
-        return false;
-      }
+  boolean isSameCharSet(String s1, int l1, int r1, String s2, int l2, int r2) {
+    if ((r1 - l1) != (r2 - l2)) return false;
+    int[] c = new int[256];
+    for (int i = l1; i < r1; i++) c[s1.charAt(i)]++;
+    for (int i = l2; i < r2; i++) c[s2.charAt(i)]--;
+    for (int i = 0; i < c.length; i++) {
+      if (c[i] != 0) return false;
     }
     return true;
   }
